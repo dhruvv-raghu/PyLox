e7f0fb1d0b96372960d47c1b60e3a1ec2b2d2441
dhruvv-raghu <raghudhruv2@gmail.com>
2025-07-17 01:38:11 +0530

    parentheses4

--- app/scan_for/parentheses.py
+++ app/scan_for/parentheses.py
@@ -16,6 +16,7 @@ class ParenthesesScanner:
     def parentheses(self):
         if self.pos+1 >= len(self.file_contents):
             raise EOFError("End of file reached")
+            return
         self.move_next()  # Let it raise EOFError naturally if needed
 
         if self.current_char == '(':
