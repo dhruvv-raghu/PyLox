10b31bf094f91807f8e670c55e02f5a1542e9d87
dhruvv-raghu <raghudhruv2@gmail.com>
2025-07-21 01:12:03 +0530

    parser uses scanner now

--- app/main.py
+++ app/main.py
@@ -10,20 +10,31 @@ def main():
     command = sys.argv[1]
     filename = sys.argv[2]
 
-    if command != "parse":
+    if command not in ['parse', 'tokenize']:
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
 
-    scanner = ParenthesesScanner(filename)
-    tokens= scanner.scan_all()
- 
-    parser= Parser(tokens)
-    ast = parser.parse()
-    print(ast)
-    
+    if command == 'tokenize':
+        scanner = ParenthesesScanner(filename)
+        tokens = scanner.scan_all()
         
-    if scanner.has_error:
-        exit(65)
+        if scanner.has_error:
+            exit(65)
+
+        for token in tokens:
+            literal_to_print = token.literal if token.literal is not None else "null"
+            print(f"{token.type} {token.lexeme} {literal_to_print}")
+        return
+    
+    if command == 'parse':
+        scanner = ParenthesesScanner(filename)
+        tokens = scanner.scan_all()
+        if scanner.has_error:
+            exit(65)
+        
+        parser = Parser(tokens)
+        ast = parser.parse()
+        print(ast)
 
 if __name__ == "__main__":
     main()
